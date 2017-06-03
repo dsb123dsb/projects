866653f32e0967cc163019d9ea99b8b877ea8221
dssb123dsb <523598438@qq.com>
2017-06-03 22:59:55 +0800

    show branch

--- wx-picsearch/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    /* 域名 */
-    //host: 'http://192.168.2.12:8080/mockjs/3',
-    //host:'http://api.treehole.org:8080/picsearch/api'
-    //host:'http://192.168.2.11:8080/picsearch/api'
-    host:'https://www.treeholeapp.com/picsearch/api'
-    //host:''
-
-};
